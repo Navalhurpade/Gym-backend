0f7a3941ea31385cd29024e98f3151138cd328f8
Naval Hurpade <navalhurpade123@gmail.com>
2023-03-10 18:36:57 +0530

    dgdsfds

--- Server.js
+++ Server.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const auth = require("./routes/auth");
-const bodyParser = require("body-parser");
-const User = require("./models/User");
-const users = require("./routes/users");
-const Payments = require("./routes/payments");
-const plan = require("./routes/plans");
-const allDate = require("./routes/allData");
-const blogs = require("./routes/blogs");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const auth = require('./routes/auth');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const users = require('./routes/users');
+const Payments = require('./routes/payments');
+const plan = require('./routes/plans');
+const allDate = require('./routes/allData');
+const blogs = require('./routes/blogs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,22 +19,22 @@ app.use(cors());
 // "mongodb://127.0.0.1:27017"
 mongoose.connect(
   process.env.DATABASE_URL,
-  { useUnifiedTopology: true, useNewUrlParser: true, dbName: "Gym" },
+  { useUnifiedTopology: true, useNewUrlParser: true, dbName: 'Gym' },
   (err) => {
-    if (!err) console.log("Connected mongodb to cluster !");
-    else throw Error("Cant connet to mongo Db");
+    if (!err) console.log('Connected mongodb to cluster !');
+    else throw Error('Cant connet to mongo Db');
   }
 );
 
-const server = app.listen(8080, () =>
-  console.log("Server is started on port 8080")
+const server = app.listen(process.env.PORT || 8080, () =>
+  console.log(`Server is started on port ${process.env.PORT || 8080}`)
 );
 
-app.use("/auth", auth);
-app.use("/users", users);
-app.use("/payments", Payments);
-app.use("/plans", plan);
-app.use("/allData", allDate);
-app.use("/blogs", blogs);
+app.use('/auth', auth);
+app.use('/users', users);
+app.use('/payments', Payments);
+app.use('/plans', plan);
+app.use('/allData', allDate);
+app.use('/blogs', blogs);
 
 module.exports = server;
